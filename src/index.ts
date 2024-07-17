7798fcbf201437e9c98704cb67d063379b54d2be
Radhi Rasho <Radhi.h@outlook.com>
2024-07-17 16:44:09 +0000

    fixed the not found route

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-import { NotFound } from "@/components";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 import { timing } from "hono/timing";
 import TodoRouter from "./todos";
 import UserRouter from "./users";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.use("*", timing({ enabled: (c) => c.req.method === "GET" }));
 app.use("*", logger());
 app.use("*", prettyJSON());
-
-app.notFound((c) => c.html(NotFound()));
+app.use(
+	"/static/*",
+	serveStatic({
+		root: "./",
+		onNotFound: (path, c) => {
+			console.log(`${path} is not found, you access ${c.req.path}`);
+		},
+	}),
+);
 
 app.onError((error, c) => {
 	if (error instanceof Error) {
@@ -21,10 +28,17 @@ app.onError((error, c) => {
 	return c.json({ error });
 });
 
+app.notFound(async (c) => {
+	const file = Bun.file("static/something.html");
+
+	const data = await file.text();
+
+	return c.render(data);
+});
+
 app.route("/users", UserRouter);
 
 app.route("/todos", TodoRouter);
-
 
 
 export default { port: 3001, fetch: app.fetch };
